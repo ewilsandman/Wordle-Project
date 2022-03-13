0cf4e9dff07511780b108718c652a0eb816e5376
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2022-03-13 18:00:15 +0100

    Almost works

--- Wordle project/Wordle project.cpp
+++ Wordle project/Wordle project.cpp
@@ -5,9 +5,9 @@
 #include <fstream>
 #include <string>
 #include <random>
+#include <vector>
 
 #define FILEPOS "../words.txt"
-
 #define FOREGROUND(color, text) "\x1B[" << static_cast<int>(color) << "m" << text << "\033[0m"
 #define BACKGROUND(color, text) "\033[3;42;" << static_cast<int>(color) << "m" << text << "\033[0m"
 
@@ -39,11 +39,11 @@ int getFileLineCount(const std::string& fileName)
     {
         while (!file.eof())
         {
-            getline(file, line); // counts the ammount of characters in file
+            getline(file, line); // counts file length
             count++;
         }
     }
-    return count; // divides by 5, the lenght of the words
+    return count;
 }
 
 std::string getLine(int lineNumber, std::string filename)
@@ -58,17 +58,94 @@ std::string getLine(int lineNumber, std::string filename)
         }
          return line;
     }
+    return nullptr;
+}
+std::vector<char> handleInput()
+{
+    std::vector<char> inputVector;
+    std::string input;
+    do
+    {
+        std::cout << "Enter a valid five letter word\n";
+        while (!(std::cin >> input))
+        {
+
+            std::cin.clear();
+            std::cin.ignore();
+            std::cout << "Invalid value, try again.\n";
+
+        }
+    } while (input.length() != 5);
+    for (size_t i = 0; i < input.length(); i++)
+    {
+        inputVector.push_back(toupper(input.at(i)));
+    }
+    return inputVector;
 }
 
 int main()
 {
+    void game();
+    game();
+}
+void game()
+{
+    std::vector<char> lineToGuessVector;
+    std::vector<char> inputVector;
+    bool running = true;
     srand(time(nullptr));
     int randomLine = rand() % getFileLineCount(FILEPOS);
     std::string lineToGuess = getLine(randomLine, FILEPOS);
+    transform(lineToGuess.begin(), lineToGuess.end(), lineToGuess.begin(), ::toupper);
 
     std::cout << FOREGROUND(ForegroundColor::BrightRed, randomLine) << std::endl;
     std::cout << FOREGROUND(ForegroundColor::BrightRed, getFileLineCount(FILEPOS)) << std::endl;
     std::cout << BACKGROUND(BackgroundColor::BrightRed, lineToGuess) << std::endl;
+    
+    for (size_t i = 0; i < lineToGuess.length(); i++)
+    {
+        lineToGuessVector.push_back(lineToGuess.at(i));
+        std::cout << lineToGuess.at(i) << std::endl;
+    }
+    while (running)
+    {
+        inputVector = handleInput();
+        int countA = 0;
+        int countB = 0;
+        bool match = false;
+        for (char a : inputVector)
+        {
+            for (char b : lineToGuessVector)
+            {
+                if (a == b)
+                {
+                    if (countA == countB)
+                    {
+                        std::cout << BACKGROUND(BackgroundColor::BrightGreen, a);
+                        match = true;
+                        a++;
+                        countA++;
+                    }
+                    else
+                    {
+                        std::cout << BACKGROUND(BackgroundColor::Yellow, a);
+                        match = true;
+                    }
+                }
+                countB++;
+            }
+            countA++;
+            if (match)
+            {
+                match = false;
+            }
+            else
+            {
+                std::cout << BACKGROUND(BackgroundColor::BrightRed, a);
+            }
+        }
+        std::cout << std::endl;
+    }
 }
 /*
 
@@ -92,7 +169,7 @@ When the player has successfully guessed the word or failed at doing so, the gam
     The code should compile without any issues.
     No runtime crashes (handle wrongful input from user).
     Read words from file.
-    Custom container for the words (motivate what structure you implemented and why it's good for this case).  (What is this?????)
+    Custom container for the words (motivate what structure you implemented and why it's good for this case). Vector good, array better if not expanding 
     Read words from file.
     Good code quality (readable, well structures and good and consistent naming).
 
