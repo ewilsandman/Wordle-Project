9908318db12ba6adb9aadcee2f519c92226591e0
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2022-03-15 16:41:59 +0100

    Now clears Godkänt criteria

--- Wordle project/Wordle project.cpp
+++ Wordle project/Wordle project.cpp
@@ -94,57 +94,104 @@ void game()
     std::vector<char> inputVector;
     bool running = true;
     srand(time(nullptr));
-    int randomLine = rand() % getFileLineCount(FILEPOS);
-    std::string lineToGuess = getLine(randomLine, FILEPOS);
-    transform(lineToGuess.begin(), lineToGuess.end(), lineToGuess.begin(), ::toupper);
-
-    std::cout << FOREGROUND(ForegroundColor::BrightRed, randomLine) << std::endl;
-    std::cout << FOREGROUND(ForegroundColor::BrightRed, getFileLineCount(FILEPOS)) << std::endl;
-    std::cout << BACKGROUND(BackgroundColor::BrightRed, lineToGuess) << std::endl;
-    
-    for (size_t i = 0; i < lineToGuess.length(); i++)
-    {
-        lineToGuessVector.push_back(lineToGuess.at(i));
-        std::cout << lineToGuess.at(i) << std::endl;
-    }
     while (running)
     {
-        inputVector = handleInput();
-        int countA = 0;
-        int countB = 0;
-        bool match = false;
-        for (char a : inputVector)
+        bool match[5]{ false,false,false,false,false };
+        int randomLine = rand() % getFileLineCount(FILEPOS);
+        std::string lineToGuess = getLine(randomLine, FILEPOS);
+        transform(lineToGuess.begin(), lineToGuess.end(), lineToGuess.begin(), ::toupper);
+        for (size_t i = 0; i < lineToGuess.length(); i++)
         {
-            for (char b : lineToGuessVector)
+            lineToGuessVector.push_back(lineToGuess.at(i));
+            std::cout << lineToGuess.at(i) << std::endl;
+        }
+        system("cls");
+        std::cout << "Welcome to scuffed Wordle" << std::endl;
+        for (size_t i = 0; i < 5; i++)
+        {
+            inputVector = handleInput();
+            int countA = 0;
+            int countB = 0;
+            int stateCount = 0;
+            int state[5] = { 0,0,0,0,0 };
+            for (char a : lineToGuessVector)
             {
-                if (a == b)
+                for (char b : inputVector)
                 {
-                    if (countA == countB)
+                    if (a == b)
                     {
-                        std::cout << BACKGROUND(BackgroundColor::BrightGreen, a);
-                        match = true;
-                        a++;
-                        countA++;
+                        if (countA == countB)
+                        {
+                            state[countA] = 1;
+                        }
+                        else
+                        {
+                            if (state[countB] != 1)
+                            {
+                                state[countB] = 2;
+                            }
+                        }
                     }
-                    else
-                    {
-                        std::cout << BACKGROUND(BackgroundColor::Yellow, a);
-                        match = true;
-                    }
+                    countB++;
                 }
-                countB++;
+                countB = 0;
+                countA++;
             }
-            countA++;
-            if (match)
+            for (int s : state)
             {
-                match = false;
+                if (s == 1)
+                {
+                    std::cout << BACKGROUND(BackgroundColor::BrightGreen, inputVector[stateCount]);
+                    match[stateCount] = true;
+                }
+                else if (s == 2)
+                {
+                    std::cout << BACKGROUND(BackgroundColor::Yellow, inputVector[stateCount]);
+                }
+                else
+                {
+                    std::cout << BACKGROUND(BackgroundColor::BrightRed, inputVector[stateCount]);
+                }
+                stateCount++;
             }
-            else
+            std::cout << std::endl;
+            std::cout << "Try " << i + 1 << "/5" << std::endl;
+        }
+        for (size_t i = 0; i < 5; i++)
+        {
+            if (match[i] != true)
             {
-                std::cout << BACKGROUND(BackgroundColor::BrightRed, a);
+                std::cout << "you loose" << std::endl;
+                break;
+            }
+            else if (i == 4)
+            {
+                std::cout << "you win" << std::endl;
             }
         }
-        std::cout << std::endl;
+        std::cout << "the word was " << lineToGuess << std::endl;
+        do
+        {
+            std::cout << "Play again?, Y or N" << std::endl;
+            std::string queryAnswer;
+            while (!(std::cin >> queryAnswer))
+            {
+
+                std::cin.clear();
+                std::cin.ignore();
+                std::cout << "Invalid value, try again.\n";
+
+            }
+
+            if (queryAnswer.find("Y") != std::string::npos)
+            {
+                break;
+            }
+            else if (queryAnswer.find("N") != std::string::npos)
+            {
+                exit(0);
+            }
+        } while (true);
     }
 }
 /*
